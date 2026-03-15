f15b84500c0a3b8ade29d69ab93425b6302445d7
fdaltro <fabriciodaltro@gmail.com>
2026-03-15 12:21:12 -0300

    Corrigindo aws_credentials

--- modules/k8s_config/main.tf
+++ modules/k8s_config/main.tf
@@ -17,7 +17,6 @@ resource "kubernetes_secret" "aws_credentials" {
     namespace = kubernetes_namespace.togglemaster.metadata[0].name
   }
 
-  # Mapeia os segredos do GitHub para as chaves que a aplicação espera
   data = {
     AWS_ACCESS_KEY_ID     = var.aws_access_key
     AWS_SECRET_ACCESS_KEY = var.aws_secret_key
--- modules/k8s_config/variables.tf
+++ modules/k8s_config/variables.tf
@@ -1,28 +1,9 @@
-variable "region" {
-  type = string
-}
+variable "region" { type = string }
+variable "rds_endpoints" { type = map(string) }
+variable "redis_endpoint" { type = string }
+variable "sqs_queue_url" { type = string }
 
-variable "rds_endpoints" {
-  type = map(string)
-}
-
-variable "redis_endpoint" {
-  type = string
-}
-
-variable "sqs_queue_url" {
-  type = string
-}
-
-# Novas variáveis para as credenciais do GitHub Actions Secrets
-variable "aws_access_key" {
-  type = string
-}
-
-variable "aws_secret_key" {
-  type = string
-}
-
-variable "aws_session_token" {
-  type = string
-}
+# Estas variáveis serão preenchidas pelo env: TF_VAR_... do GitHub Actions
+variable "aws_access_key" { type = string }
+variable "aws_secret_key" { type = string }
+variable "aws_session_token" { type = string }
