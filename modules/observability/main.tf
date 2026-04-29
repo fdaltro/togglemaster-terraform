fa7126151168203cf8f5e2f2f1d5d29bc440f4aa
Fabricio Daltro <fabriciodaltro@gmail.com>
2026-04-28 22:23:40 -0300

    Adicionando discos persistentes

--- modules/observability/main.tf
+++ modules/observability/main.tf
@@ -1,58 +1,90 @@
-
-# Criação do Namespace isolado para a stack de monitoramento
+# 1. Namespace isolado para a stack de monitoramento
 resource "kubernetes_namespace" "monitoring" {
   metadata {
     name = "observabilidade"
   }
 }
 
-# Prometheus: Coleta de métricas da infraestrutura e pods.
+# 2. Prometheus: Coleta de métricas da infraestrutura e pods
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
+  # Ativando persistência para o servidor de métricas
   set {
     name  = "server.persistentVolume.enabled"
-    value = "false"
+    value = "true"
   }
   set {
-    name  = "alertmanager.persistentVolume.enabled"
-    value = "false"
+    name  = "server.persistentVolume.storageClass"
+    value = "gp2"
+  }
+  set {
+    name  = "server.persistentVolume.size"
+    value = "8Gi"
   }
 
+  # Ativando persistência para o Alertmanager
+  set {
+    name  = "alertmanager.persistentVolume.enabled"
+    value = "true"
+  }
+  set {
+    name  = "alertmanager.persistentVolume.storageClass"
+    value = "gp2"
+  }
+
+  # Ativando persistência para o Pushgateway
   set {
     name  = "pushgateway.persistentVolume.enabled"
-    value = "false"
+    value = "true"
+  }
+  set {
+    name  = "pushgateway.persistentVolume.storageClass"
+    value = "gp2"
   }
 }
 
-# Loki: Centralização e indexação de logs
+# 3. Loki: Centralização e indexação de logs
 resource "helm_release" "loki" {
   name       = "loki"
   repository = "https://grafana.github.io/helm-charts"
-  chart      = "loki-stack" 
+  chart      = "loki-stack" # Inclui Promtail para coleta automática
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
   set {
     name  = "loki.persistence.enabled"
-    value = "false"
+    value = "true"
+  }
+  set {
+    name  = "loki.persistence.storageClass"
+    value = "gp2"
+  }
+  set {
+    name  = "loki.persistence.size"
+    value = "5Gi"
   }
 }
 
-# Grafana: Dashboards e visualização de dados
+# 4. Grafana: Dashboards e visualização de dados [cite: 34, 38]
 resource "helm_release" "grafana" {
   name       = "grafana"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
-  # Configurações de persistência e senha
+  # Ativando persistência para manter Dashboards criados [cite: 39]
   set {
     name  = "persistence.enabled"
-    value = "false"
+    value = "true"
   }
+  set {
+    name  = "persistence.storageClass"
+    value = "gp2"
+  }
+
   set {
     name  = "adminPassword"
     value = "admin123"
