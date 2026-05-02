ffb1fe1d8c65e029c34174697fdb92c4218b55be
Fabricio Daltro <fabriciodaltro@gmail.com>
2026-05-01 22:11:06 -0300

    Discos sem PVC

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -18,43 +18,41 @@ resource "aws_eks_cluster" "main" {
 
 # ==========================================================
 # 2. ADD-ONS ESSENCIAIS (REDE E DNS)
-# Mantemos apenas o básico para o cluster operar sem erros de comunicação.
 # ==========================================================
 
 # VPC CNI: Responsável por atribuir IPs da VPC aos Pods
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name                = aws_eks_cluster.main.name
   addon_name                  = "vpc-cni"
-  resolve_conflicts_on_update = "OVERWRITE" # Resolve conflitos de permissão no Academy[cite: 9]
+  resolve_conflicts_on_update = "OVERWRITE"
   depends_on                  = [aws_eks_cluster.main]
 }
 
-# Kube-Proxy: Gerencia as regras de rede (Services) do Kubernetes[cite: 9]
+# Kube-Proxy: Gerencia as regras de rede (Services) do Kubernetes
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name                = aws_eks_cluster.main.name
   addon_name                  = "kube-proxy"
-  resolve_conflicts_on_update = "OVERWRITE"[cite: 9]
+  resolve_conflicts_on_update = "OVERWRITE"
   depends_on                  = [aws_eks_cluster.main]
 }
 
-# CoreDNS: Sistema de nomes interno (essencial para microsserviços se acharem)[cite: 9]
+# CoreDNS: Sistema de nomes interno
 resource "aws_eks_addon" "coredns" {
   cluster_name                = aws_eks_cluster.main.name
   addon_name                  = "coredns"
-  resolve_conflicts_on_update = "OVERWRITE"[cite: 9]
+  resolve_conflicts_on_update = "OVERWRITE"
   
-  # O CoreDNS só estabiliza após os nós (Worker Nodes) estarem prontos[cite: 9]
+  # O CoreDNS só estabiliza após os nós estarem prontos
   depends_on = [aws_eks_node_group.main] 
 }
 
 # ==========================================================
 # 3. NODE GROUP (WORKER NODES)
-# As máquinas onde seus containers realmente serão executados.
 # ==========================================================
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "${var.cluster_name}-node-group"
-  node_role_arn   = var.lab_role_arn # Utilizando a LabRole para permissões de execução[cite: 9]
+  node_role_arn   = var.lab_role_arn
   subnet_ids      = var.subnet_ids
 
   scaling_config {
@@ -63,7 +61,6 @@ resource "aws_eks_node_group" "main" {
     min_size     = 1
   }
 
-  # t3.medium é o equilíbrio ideal para rodar 5 microsserviços + ArgoCD[cite: 9]
   instance_types = ["t3.medium"] 
 
   tags = {
