059443c42f1cc0c5e6e80b5fe5ac6e96da7238b2
Fabricio Daltro <fabriciodaltro@gmail.com>
2026-05-02 09:05:05 -0300

    Local Path Provisioner v2

--- modules/k8s_config/main.tf
+++ modules/k8s_config/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+  }
+}
+
 # ==========================================================
 # 1. CRIAÇÃO DO NAMESPACE
 # ==========================================================
