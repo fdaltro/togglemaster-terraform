1d942b274ec8da9f2e4b56da65d2393fe6bf23c7
Fabricio Daltro <101665130+fdaltro@users.noreply.github.com>
2026-05-04 15:40:17 -0300

    Atualização Driver de Instância Local

--- modules/observability/main.tf
+++ modules/observability/main.tf
@@ -5,7 +5,18 @@ resource "kubernetes_namespace" "monitoring" {
   }
 }
 
-# 2. Prometheus: Coleta de métricas e Alertmanager
+# 2. StorageClass para o Driver de Instância Local (EC2 Instance Store)
+# Esta classe utiliza o driver que você ativou manualmente no console
+resource "kubernetes_storage_class" "local_instance_sc" {
+  metadata {
+    name = "local-instance-sc"
+  }
+  storage_provisioner    = "instance-store.csi.aws.com" 
+  reclaim_policy         = "Delete"
+  volume_binding_mode    = "WaitForFirstConsumer"
+}
+
+# 3. Prometheus: Coleta de métricas e Alertmanager
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
@@ -13,15 +24,28 @@ resource "helm_release" "prometheus" {
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
   timeout    = 600
 
-  # Desativando persistência para evitar erros de PVC no AWS Academy
+  # Persistência do Prometheus Server usando o driver local
   set {
     name  = "server.persistentVolume.enabled"
     value = "true"
   }
+  set {
+    name  = "server.persistentVolume.storageClass"
+    value = "local-instance-sc"
+  }
+  set {
+    name  = "server.persistentVolume.size"
+    value = "8Gi"
+  }
 
+  # Persistência do Alertmanager usando o driver local
   set {
     name  = "alertmanager.persistentVolume.enabled"
     value = "true"
+  }
+  set {
+    name  = "alertmanager.persistentVolume.storageClass"
+    value = "local-instance-sc"
   }
 
   set {
@@ -29,7 +53,7 @@ resource "helm_release" "prometheus" {
     value = "true"
   }
 
-  # RESOLUÇÃO DO ERRO: Injeção de configuração básica para o Alertmanager não dar Crash
+  # Injeção de configuração básica para evitar o erro de arquivo ausente no Alertmanager
   values = [
     yamlencode({
       alertmanager = {
@@ -55,7 +79,7 @@ resource "helm_release" "prometheus" {
   ]
 }
 
-# 3. Loki: Agregador de Logs
+# 4. Loki: Agregador de Logs
 resource "helm_release" "loki" {
   name       = "loki"
   repository = "https://grafana.github.io/helm-charts"
@@ -67,9 +91,17 @@ resource "helm_release" "loki" {
     name  = "loki.persistence.enabled"
     value = "true"
   }
+  set {
+    name  = "loki.persistence.storageClass"
+    value = "local-instance-sc"
+  }
+  set {
+    name  = "loki.persistence.size"
+    value = "5Gi"
+  }
 }
 
-# 4. Grafana: Dashboards e Visualização
+# 5. Grafana: Dashboards e Visualização
 resource "helm_release" "grafana" {
   name       = "grafana"
   repository = "https://grafana.github.io/helm-charts"
@@ -81,13 +113,17 @@ resource "helm_release" "grafana" {
     name  = "persistence.enabled"
     value = "true"
   }
+  set {
+    name  = "persistence.storageClass"
+    value = "local-instance-sc"
+  }
 
   set {
     name  = "adminPassword"
     value = "admin123"
   }
 
-  # Injeção automática das fontes de dados (Prometheus e Loki)
+  # Configuração automática de Data Sources
   values = [
     yamlencode({
       datasources = {
