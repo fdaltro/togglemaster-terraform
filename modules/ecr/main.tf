c767deb04a3a6be8398a780f4cdf9eb9b2053ed0
Fabricio Daltro <101665130+fdaltro@users.noreply.github.com>
2026-05-06 17:07:15 -0300

    Atualização para forçar deleção de repositorio mesmo com imagem

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,10 +1,12 @@
 resource "aws_ecr_repository" "microservices" {
   for_each             = toset(var.service_names)
   name                 = "${var.project_name}-${each.value}"
-  image_tag_mutability = "IMMUTABLE" # Garante que as tags (commit hash) não sejam sobrescritas [cite: 63]
+  image_tag_mutability = "IMMUTABLE" 
+
+  force_delete = true 
 
   image_scanning_configuration {
-    scan_on_push = true # Requisito de scan de vulnerabilidades 
+    scan_on_push = true 
   }
 
   tags = {
