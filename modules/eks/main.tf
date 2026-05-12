17b3aaf38b66918cf735090e70111950941c59b7
Fabricio Daltro <101665130+fdaltro@users.noreply.github.com>
2026-05-12 10:47:21 -0300

    Aumento de quantidade de máquinas no nó EKS

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -26,8 +26,8 @@ resource "aws_eks_node_group" "main" {
   subnet_ids      = var.subnet_ids
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 3
+    max_size     = 4
     min_size     = 1
   }
 
