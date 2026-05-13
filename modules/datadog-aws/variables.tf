89c83b3b798b4a05ff1118e273fb91bf2c7c6021
Fabricio Daltro <101665130+fdaltro@users.noreply.github.com>
2026-05-13 16:11:26 -0300

    Update variables.tf

--- modules/datadog-aws/variables.tf
+++ modules/datadog-aws/variables.tf
@@ -1,21 +1,4 @@
-variable "datadog_api_key" {
-  type        = string
-  description = "Datadog API Key"
-}
-
-variable "datadog_app_key" {
-  type        = string
-  description = "Datadog App Key"
-}
-
 variable "aws_account_id" {
-  type        = string
-  description = "ID da conta AWS Academy"
-  default     = "504491092699"
-}
-
-variable "lab_role_name" {
-  type        = string
-  description = "Nome da role pré-existente na Academy"
-  default     = "LabRole"
+  type    = string
+  default = "504491092699"
 }
