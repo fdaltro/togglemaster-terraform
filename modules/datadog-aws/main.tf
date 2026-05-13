8967a3b49b857f2b0bce771df1754872a6accc56
Fabricio Daltro <101665130+fdaltro@users.noreply.github.com>
2026-05-13 16:10:39 -0300

    Update main.tf datadog

--- modules/datadog-aws/main.tf
+++ modules/datadog-aws/main.tf
@@ -6,14 +6,31 @@ terraform {
   }
 }
 
-# Configura a integração no lado do Datadog
-resource "datadog_integration_aws" "core" {
-  account_id = var.aws_account_id
-  role_name  = var.lab_role_name
+# Este recurso configura o Datadog para "olhar" para sua conta AWS
+resource "datadog_integration_aws_account" "datadog_integration" {
+  aws_account_id = var.aws_account_id
+  aws_partition  = "aws"
   
-  # Como não podemos editar a Trust Relationship da LabRole, 
-  # desativamos a verificação de STS se necessário (opção dependente da versão do provider)
-  host_resource_collection_enabled = true
-  
-  filter_tags = ["project:togglemaster"]
+  aws_regions {
+    include_all = true
+  }
+
+  auth_config {
+    aws_auth_config_role {
+      # Na Academy, usamos a role que já existe
+      role_name = "LabRole" 
+    }
+  }
+
+  resources_config {
+    cloud_security_posture_management_collection = false # Desativado para evitar erros de permissão na Academy
+    extended_collection                          = true
+  }
+
+  metrics_config {
+    namespace_filters {
+      # Filtra apenas o que importa para seus 5 serviços
+      include_only = ["AWS/SQS", "AWS/DynamoDB", "AWS/ElastiCache", "AWS/Lambda"]
+    }
+  }
 }
